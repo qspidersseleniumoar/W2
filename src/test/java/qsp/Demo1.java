ef004754660176c601b49193fa3022c79ba76c45
Kishore <Kishore@DESKTOP-GDFIP89>
2019-11-26 19:26:52 +0530

    eded

---
+++ src/test/java/qsp/Demo1.java
@@ -0,0 +1,22 @@
+package qsp;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void test1(){
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test2(){
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test3(){
+		System.out.println("test1");
+	}
+
+}
